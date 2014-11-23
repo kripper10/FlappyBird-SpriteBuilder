8fabb86a79f8dccdd9eb4477c1b6471fbc9700c2
Chris Thomas <thomaschris2013@gmail.com>
2014-11-23 21:38:43 +0000

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -15,6 +15,9 @@
     [physicsNode addChild:character];
 }
 
-// put new methods here
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    // this will get called every time the player touches the screen
+    [character flap];
+}
 
 @end
